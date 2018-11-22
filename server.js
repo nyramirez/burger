a4a3c5c2fd6d80fecc59e34ee485129c10bf4efa
Nefas <nyramirez@gmail.com>
2018-11-21 20:21:52 -0600

    still not displaying

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+require("dotenv").config();
+
 let express = require("express");
 let app = express();
 
 let PORT = process.env.DB_PORT || 8080;
 
-app.get("/", function (req, res) {
-    res.send("eat the burger page server running");
-});
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+let routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
 
 app.listen(PORT, function () {
     console.log("App listening on http://localhost:" + PORT);
